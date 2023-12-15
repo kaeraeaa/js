b40c32e82eddcb4940cd25a839a5fda5bab4d39c
kaeraeaa <129053421+kaeraeaa@users.noreply.github.com>
2023-12-15 19:12:46 +0200

    comit

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
-const userName = "lera";
-console.log(userName);
+// const message = 'hello';
+// console.log(message.length);
+// console.log(message.indexOf('q'));
+
+//  const firstName = 'lera';
+//  const lastName = ' kochuma';
+//  const fullname = `hello, ${firstName} ${lastName}`;
+//  console.log(fullname);
+
+// const firstName = 'Mango';
+// const lastName = 'Lee';
+// const room = 716;
+// const type = 'VIP';
+
+// const customersMsg = `Гість ${firstName} ${lastName} поселяється в ${type} номер ${room}`;
+
+// console.log(customersMsg);
+
+
+const firstllame = prompt("Your name");
+const lasthame = prompt("Your surname");
+const age = prompt('Your age');
+const text = "Користувач ${firstName} ${lastName} віком ${age} має доступ до аккаутну!";
+console.log(text);
