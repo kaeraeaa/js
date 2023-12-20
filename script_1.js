fefa2f4d67c8e4f2897f68e4e267becd84f32997
kaeraeaa <129053421+kaeraeaa@users.noreply.github.com>
2023-12-20 11:32:01 +0200

    comit

---
+++ script_1.js
@@ -0,0 +1,29 @@
+const age = 15; 
+console.log(age);
+
+const name = 'Lera';
+console.log(name);
+
+
+const isStudent = true; 
+console.log('Ви студент?', isStudent);
+
+
+const myString = "Моя улюблена цитата";
+console.log(myString);
+
+
+
+const myNumber = 5;
+console.log(myNumber + 10);
+
+
+const myNull = null;
+console.log(myNull);
+
+
+const userName = prompt('Введіть ваше імя');
+alert('Привіт, ' + userName + '!');
+
+
+// Не зрозуміла як зробити 8 і 9 завдання
